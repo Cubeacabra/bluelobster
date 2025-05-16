4feb7bf5981de1757ce0133de48460dccacabec8
Ethan <116187182+Damadog101@users.noreply.github.com>
2025-05-16 13:58:21 -0700

    Update jumpscare.js

--- jumpscare.js
+++ jumpscare.js
@@ -1,7 +1,24 @@
-const LOBSTER_JUMPSCARE = new Audio("lobsterSounds.mp3");
+/*const LOBSTER_JUMPSCARE = new Audio("lobsterSounds.mp3");
 
 function playAudio() {
 	LOBSTER_JUMPSCARE.play();
 }
 
 playAudio();
+*/
+
+let lobsters = document.querySelectorAll('img');
+let currentSpeed = 6;
+
+let button = document.getElementById("button");
+
+
+function cow() {
+	for (int i = 0; i < lobsters.length; i++) {
+		currentSpeed -= 0.5;
+		lobsters[i].style.animationDuration = currentSpeed + 's';
+	} 
+}
+button.addEventListener('click', cow);
+
+
